369b12306411fb64bbddda7f3a9b1cde13fcc2ce
lrplrplrp <2833784318@qq.com>
2024-08-25 10:53:25 +0800

    Update loads.js

--- loads.js
+++ loads.js
@@ -27,9 +27,9 @@ function loadExternalResource(url, type) {
 async function loadScriptsInOrder(){
 	if (screen.width >= 768) {//使用宽度判断设备是否适合加载
 	await Promise.all([
-			loadExternalResource(live2d_path + "pixi.min.js", "js"),
-			loadExternalResource(live2d_path + "live2d.min.js", "js"),
-			loadExternalResource(live2d_path + "live2dcubismcore.min.js", "js"),
+			loadExternalResource("https://raw.bgithub.xyz/lrplrplrp/bkyl2d/main/pixi.min.js", "js"),
+			loadExternalResource("https://raw.bgithub.xyz/lrplrplrp/bkyl2d/main/live2d.min.js", "js"),
+			loadExternalResource("https://raw.bgithub.xyz/lrplrplrp/bkyl2d/main/live2dcubismcore.min.js", "js"),
 			loadExternalResource("https://cdn.bootcss.com/jquery/1.11.3/jquery.min.js", "js"),
 			loadExternalResource(live2d_path + "waifu.css", "css"),
 			loadExternalResource("https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css", "css")

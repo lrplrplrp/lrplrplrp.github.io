56667a7c7f73addb79409a4b79e0b8fc3a56f84e
lrplrplrp <2833784318@qq.com>
2024-08-25 11:11:05 +0800

    Update loads.js

--- loads.js
+++ loads.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://gitee.com/lrplrplrp/live2d/raw/main/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/lrplrplrp/live2d@main/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
